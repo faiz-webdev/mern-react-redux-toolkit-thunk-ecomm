441c5b9dbf5cc58f3c639308336850fc6a4cddaa
faiz-ttnew <faijuddin@tothenew.com>
2023-08-16 15:08:53 +0530

    6. show product list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Counter from "./features/counter/Counter";
 import "./App.css";
+import ProductList from './features/product-list/ProductList';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <ProductList />
     </div>
   );
 }
