0ce266ece017d726cf02a0e3f0ab3e23eb296f52
faiz-ttnew <faijuddin@tothenew.com>
2023-08-16 17:00:15 +0530

    12. added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,34 @@ import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 
+// import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+]);
+
 function App() {
   return (
     <div className="App">
-      <SignupPage></SignupPage>
+      <RouterProvider router={router} />
     </div>
   );
 }
