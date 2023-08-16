0fd087418450460d36276a3cfdf6b796f1d61161
faiz-ttnew <faijuddin@tothenew.com>
2023-08-16 14:37:41 +0530

    5. product list slice

--- frontend/src/features/product-list/ProductList.js
+++ frontend/src/features/product-list/ProductList.js
@@ -3,6 +3,20 @@ import { useSelector, useDispatch } from "react-redux";
 import { increment, incrementAsync, selectCount } from "./productListSlice";
 
 export default function ProductList() {
+  const products = [
+    {
+      id: 1,
+      name: "Basic Tee",
+      href: "#",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: "$35",
+      color: "Black",
+    },
+    // More products...
+  ];
+
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
 
