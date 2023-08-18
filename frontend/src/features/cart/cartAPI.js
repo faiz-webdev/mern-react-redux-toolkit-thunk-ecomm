ce32e030f3f4da37c5e4b99e92285d0fc44c2fd3
faiz-ttnew <faijuddin@tothenew.com>
2023-08-18 17:13:58 +0530

    36. update quantity and remove item from cart

--- frontend/src/features/cart/cartAPI.js
+++ frontend/src/features/cart/cartAPI.js
@@ -20,3 +20,30 @@ export function fetchItemsByUserId(userId) {
     resolve({ data });
   });
 }
+
+export function updateCart(update) {
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8090/cart/" + update.id, {
+      method: "PATCH",
+      body: JSON.stringify(update),
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function deleteItemFromCart(itemId) {
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8090/cart/" + itemId, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    const data = await response.json();
+    resolve({ data: { id: itemId } });
+  });
+}
